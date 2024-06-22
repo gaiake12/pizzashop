845f2f299c31f6ebdf29210aa1c73dbbb0c386d6
gaiake12 <gaiake12@hotmail.com>
2024-06-22 10:18:50 -0300

    feat: first commit

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,42 @@
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Pizza Shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Matheus Oliveira</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            matheus.oliveira@hotmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuItem className="cursor-pointer">
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da Loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
